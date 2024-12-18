eca516778e1eeb308be2c4ed02784cbf85cad3b3
07krW5Hnr5ghy <jryn145@gmail.com>
2024-12-17 23:24:56 -0500

    refactor(main): create singleton database connection for postgreSQL and mongoDB

---
+++ configs/mongo.js
@@ -0,0 +1,40 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+const {MONGO_URI,MONGO_DB_NAME} = process.env;
+
+class MongoSingleton{
+    constructor(){
+        if(!MongoSingleton.instance){
+            this.client = new MongoClient(MONGO_URI);
+            MongoSingleton.instance = this;
+        }
+        return MongoSingleton.instance;
+    }
+    async connect(){
+        try{
+            if(!this.db){
+                await this.client.connect();
+                this.db = this.client.db(MONGO_DB_NAME);
+                console.log("MongoDB connected successfully.");
+            }
+            return this.db;
+        }catch(error){
+            console.error("Error connecting to MongoDB:",error);
+            throw error;
+        }
+    }
+    async close(){
+        try{
+            await this.client.close();
+            console.log("MongoDB connected closed.");
+        }catch(error){
+            console.error("Error closing MongoDB connection:",error);
+        }
+    }
+}
+
+const mongoInstance = new MongoSingleton();
+Object.freeze(mongoInstance);
+export default mongoInstance;
---
+++ configs/postgres.js
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
+dotenv.config();
+const {PG_USER,PG_HOST,PG_DB,PG_PASSWORD,PG_PORT} = process.env;
+
+class PostgresSingleton{
+    constructor(){
+        if(!PostgresSingleton.instance){
+            this.sequelize = new Sequelize(
+                PG_DB,
+                PG_USER,
+                PG_PASSWORD,
+                {
+                    host:PG_HOST,
+                    port:PG_PORT,
+                    dialect:"postgres",
+                    logging:true
+                }
+            );
+            PostgresSingleton.instance = this;
+        }
+        return PostgresSingleton.instance;
+    }
+    async connect(){
+        try{
+            await this.sequelize.authenticate();
+            console.log("PostgreSQL connected successfully.");
+        }catch(error){
+            console.error("Error connecting to PostgresSQL:", error);
+            throw error;
+        }
+    }
+    getSequelize(){
+        return this.sequelize;
+    }
+}
+
+const postgresInstance = new PostgresSingleton();
+Object.freeze(postgresInstance);
+export default postgresInstance;
